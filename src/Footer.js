ed1b88c38ea95cd3e05f9105a2573e1035b80af6
Vedina2022 <2014vedina@gmail.com>
2022-09-07 17:37:15 -0600

    Added content to Footer component, background image

--- src/Footer.js
+++ src/Footer.js
@@ -1,3 +1,19 @@
+import "./Footer.css";
+
 export default function Footer() {
-  return <div>Hello from Footer</div>;
+  return (
+    <div className="footer">
+      <p className="developer" id="developer">
+        <a
+          href="https://github.com/Vedina2022/Weather-forecast-app.git"
+          target="_blank"
+          rel="noreferrer"
+          className="developer-link"
+        >
+          Open-source code
+        </a>
+        <span> by Nadiia Kyshinska</span>
+      </p>
+    </div>
+  );
 }
