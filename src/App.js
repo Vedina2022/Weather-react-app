4c96936a3c21cdbf5aa330e6858276caf421346a
Vedina2022 <2014vedina@gmail.com>
2022-09-15 22:26:33 -0600

    Excluded DailyWeather component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import CurrentWeather from "./CurrentWeather";
-import DailyWeather from "./DailyWeather";
+/* import DailyWeather from "./DailyWeather"; */
 import Footer from "./Footer";
 
 function App() {
   return (
     <div className="container">
       <CurrentWeather defaultCity="Mykolaiv" />
-      <DailyWeather />
+      {/* <DailyWeather /> */}
       <Footer />
     </div>
   );
